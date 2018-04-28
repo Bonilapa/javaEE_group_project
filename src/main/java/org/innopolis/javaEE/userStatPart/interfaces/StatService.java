b1eec1d8fb9c3709bd481252bd327a8edeb9841a
Grigory Shvedov <g.shvedov@innopolis.com>
2018-04-28 17:12:29 +0300

    stat part upd

--- src/main/java/org/innopolis/javaEE/userStatPart/interfaces/StatService.java
+++
@@ -1,7 +0,0 @@
-//package org.innopolis.javaEE.userStatPart.interfaces;
-//
-//import org.innopolis.javaEE.userStatPart.model.Stat;
-//
-//public interface StatService {
-//    Stat getUserStat(Stat stat);
-//}
