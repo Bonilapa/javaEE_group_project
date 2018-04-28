00681b7e52df27f34243ca598bc0e2f32b686816
Grigory Shvedov <g.shvedov@innopolis.com>
2018-04-28 16:37:47 +0300

    Stat Part Commit

---
+++ src/main/java/org/innopolis/javaEE/userStatPart/model/Top.java
@@ -0,0 +1,50 @@
+package org.innopolis.javaEE.userStatPart.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tops")
+public class Top {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Aid;
+
+    @Column(name = "userId")
+    private Long uid;
+
+    @Column(name = "rank")
+    private int rank;
+
+    public Top() {}
+
+    public Top(Long aid, Long uid, int rank) {
+        this.Aid = aid;
+        this.uid = uid;
+        this.rank = rank;
+    }
+
+    public Long getAid() {
+        return Aid;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setAid(Long aid) {
+        Aid = aid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+}
